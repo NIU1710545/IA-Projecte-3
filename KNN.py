bac4550b7febe8c5d0047255f70f81a09fa5847b
NIU1710545 <149046590+NIU1710545@users.noreply.github.com>
2025-05-12 08:54:20 +0200

    Update KNN.py
    Modificació en l'espai de característiques del codi KNN.py

--- KNN.py
+++ KNN.py
@@ -8,25 +8,43 @@ from scipy.spatial.distance import cdist
 
 
 class KNN:
-    def __init__(self, train_data, labels):
+    def __init__(self, train_data, labels, distance_metric='euclidean'):
         self._init_train(train_data)
         self.labels = np.array(labels)
         self.neighbors = None
+        self.distance_metric = distance_metric
         
         
         #############################################################
         ##  THIS FUNCTION CAN BE MODIFIED FROM THIS POINT, if needed
         #############################################################
 
-    def _init_train(self, train_data):
+    def _init_train(self, train_data, feature_type='all'):
         """
         initializes the train data
         :param train_data: PxMxNx3 matrix corresponding to P color images
         :return: assigns the train set to the matrix self.train_data shaped as PxD (P points in a D dimensional space)
         """
+        # Convertir a float i reshape a PxD
         train_data = train_data.astype(np.float32)
-        P, M, N = train_data.shape 
-        self.train_data = train_data.reshape(P, M * N)
+
+        # Se'ns proposa mirar de modificar l'espai de característiques
+        if feature_type == 'all':
+            # tots els pixels
+            self.train_data = self.train_data.reshape(self.train_data.shape[0], -1)
+        elif feature_type == 'upper_half':
+            # Només la meitat superior 
+            h = self.train_data.shape[1] // 2
+            self.train_data = self.train_data[:, :h, :].reshape(self.train_data.shape[0], -1)
+        elif feature_type == 'lower_half':
+            h = self.train_data.shape[1] // 2
+            self.train_data = self.train_data[:, h:, :].reshape(self.train_data.shape[0], -1)
+        elif feature_type == 'mean':
+            # Valor mitjà
+            self.train_data = np.mean(self.train_data, axis=(1,2))
+        elif feature_type == 'std':
+            # Desviació estàndard
+            self.train_data = np.std(self.train_data, axis=(1,2))
 
     def get_k_neighbours(self, test_data, k):
         test_data = test_data.astype(np.float32)
